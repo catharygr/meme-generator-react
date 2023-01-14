2a9ecd0303092d294e95f8f1001a7c066f219a39
cathy <cathy@bubulazi.com>
2023-01-14 20:04:52 +0000

    Generador de meme, componente Header terminado

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Header from './componentes/Header'
+import Meme from './componentes/Meme'
+import './App.css'
+
+ export default function App() {
+    return (
+      <>
+      <Header />
+      <Meme />
+      </>
+    )
+  }
---
+++ src/componentes/Meme.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Meme() {
+  return (
+    <main>
+      <form className='form'>
+        <input 
+            type='text'
+            placeholder='Top text'
+            className='form--input'
+          />
+          <input 
+            type='text'
+            placeholder='Botton-text'
+            className='form--input'
+          />
+          <button 
+          
+              className='form-button'
+              >
+              Get a new meme image 🖼
+
+          </button>
+
+      </form>
+    </main>
+  )
+}
